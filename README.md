0a12bd0dc66e448c12cbe5cdaf38c4fec3d4c138
Lawrence H <lawrence_huang1@brown.edu>
2023-12-02 03:41:42 -0500

    Add basic README and show Whisper model in placeholder text

---
+++ README.md
@@ -0,0 +1,25 @@
+# Whisper Test Python UI
+
+## Installation
+
+- tested with Python version `3.9.18`
+- `pip install -r requirements.txt`
+- run with `python whisper_gui.py`
+
+## Features
+
+- simple audio transcription UI inspired by [MacWhisper](https://goodsnooze.gumroad.com/l/macwhisper), utilizes the [`faster-whisper`](https://github.com/SYSTRAN/faster-whisper) backend
+- can modify `model_size` parameter in code to use all Whisper model sizes for free (MacWhisper limits to small, tiny, or base models)
+
+![](assets/v1_ui.png)
+
+## Future features:
+
+- [ ] streaming - e.g. https://github.com/ufal/whisper_streaming
+- [ ] have a separate section where you record some context for post-processing by another LLM (either API or local)
+  - e.g. if you had a meeting, tell other LLM that the audio file was a transcript of a call, process it into key takeaways and action items
+
+## UI improvements
+
+- [ ] Model selection dropdown
+- [ ] Recording dot animation when recording
--- whisper_gui.py
+++ whisper_gui.py
@@ -8,6 +8,7 @@ from PyQt6.QtCore import pyqtSlot, QThread, pyqtSignal
 import sounddevice as sd
 from faster_whisper import WhisperModel
 
+
 class TranscriptionThread(QThread):
     transcription_done = pyqtSignal(str)
 
@@ -20,20 +21,24 @@ class TranscriptionThread(QThread):
         # Transcribe the recording
         segments, info = self.model.transcribe(self.audio_file)
         # Concatenate the text from each segment to form the full transcript
-        full_transcript = '\n'.join([segment.text.strip() for segment in segments])
-        self.transcription_done.emit(full_transcript)  # Emit the signal with the transcript
+        full_transcript = '\n'.join([segment.text.strip()
+                                    for segment in segments])
+        # Emit the signal with the transcript
+        self.transcription_done.emit(full_transcript)
+
 
 class AudioRecorder(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
 
         self.is_recording = False
         self.audio_buffer = []
         self.sample_rate = 16000
 
-        self.model_size = "large-v2"
+        self.model_size = "large-v3"
         self.model = WhisperModel(self.model_size, compute_type="int8")
+
+        self.initUI()
 
     def initUI(self):
         self.setWindowTitle('Audio Recorder')
@@ -51,7 +56,7 @@ class AudioRecorder(QMainWindow):
         self.transcript_text = QTextEdit(self)
         self.transcript_text.setReadOnly(True)
         layout.addWidget(self.transcript_text)
-        self.transcript_text.setPlaceholderText("Transcript will appear here")
+        self.reset_placeholder_text()
 
         # Set layout
         central_widget = QWidget()
@@ -73,8 +78,12 @@ class AudioRecorder(QMainWindow):
         clipboard.setText(self.transcript_text.toPlainText())
 
     def clear_text(self):
-        self.transcript_text.setPlaceholderText("")
         self.transcript_text.clear()
+        self.reset_placeholder_text()
+
+    def reset_placeholder_text(self):
+        self.transcript_text.setPlaceholderText(
+            f"Using model {self.model_size}. Transcript will appear here once recording is complete...")
 
     def toggle_recording(self):
         if self.is_recording:
@@ -103,23 +112,28 @@ class AudioRecorder(QMainWindow):
                 wf.setnchannels(1)
                 wf.setsampwidth(2)
                 wf.setframerate(self.sample_rate)
-                wf.writeframes(np.int16(np.concatenate(self.audio_buffer) * 32767))
+                wf.writeframes(
+                    np.int16(np.concatenate(self.audio_buffer) * 32767))
             print(f"Recording saved to {temp_file.name}")
 
             # Start the transcription in a separate thread
-            self.transcription_thread = TranscriptionThread(temp_file.name, self.model)
-            self.transcription_thread.transcription_done.connect(self.append_transcription)
+            self.transcription_thread = TranscriptionThread(
+                temp_file.name, self.model)
+            self.transcription_thread.transcription_done.connect(
+                self.append_transcription)
             self.transcription_thread.start()
 
     @pyqtSlot(str)
     def append_transcription(self, transcript):
         self.transcript_text.append(transcript)
 
+
 def main():
     app = QApplication(sys.argv)
     ex = AudioRecorder()
     ex.show()
     sys.exit(app.exec())
+
 
 if __name__ == '__main__':
     main()
